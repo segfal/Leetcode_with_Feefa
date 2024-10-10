8c2bf213fcb6d869b857f1e10053a26def045dcc
segfal <segfaultboi@gmail.com>
2024-10-09 23:29:54 -0400

    first commit

---
+++ Sliding_wIndow/findMaxAverage.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+    //looking for the maximum average subarray
+    
+    /*  12 , 1, 2, 4, 11
+        15/3
+    
+        the k tells u how long the subarray should be
+        nums is the array
+    
+        we need the slice to get from one element to another the sliding window way
+        and then after we calculate k elements, 
+        then we want to move on from the past elements
+        and subtract them so we dont keep adding those values
+    
+        -average variable, maximum variable
+    
+        we wanna go through the array until k elements and
+            - find the average and then work on finding the maximum average
+    */
+        let average = nums.slice(0, k).reduce((a,b) => a+b);
+        let maxSum=average;
+        
+        for (let i = k; i<nums.length; i++){
+            average += parseFloat(nums[i]) / parseFloat(k);
+            average = nums[i-k];
+            maxSum = Math.max(average)
+        } 
+        console.log(maxSum) /// 
+        return maxSum;
+    
+    };
+    
+    
+    k=4
+    // 12.75000
+    findMaxAverage([1,12,-5,-6,50,3],4)
